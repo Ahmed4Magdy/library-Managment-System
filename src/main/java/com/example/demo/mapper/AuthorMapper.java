f6104ad1c23d1599e60df4cc75a15ba4731c5e48
Ahmed4Magdy <Ahmed@example.com>
2025-10-29 19:04:30 +0300

    dto-mapper

--- src/main/java/com/example/demo/mapper/AuthorMapper.java
+++ src/main/java/com/example/demo/mapper/AuthorMapper.java
@@ -1,9 +1,12 @@
 package com.example.demo.mapper;
 
 import com.example.demo.Dto.AuthorDto;
+import com.example.demo.Dto.BookDto;
 import com.example.demo.entity.Author;
+import com.example.demo.entity.Book;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
@@ -15,4 +18,8 @@ public interface AuthorMapper {
   @Mapping(target = "id", ignore = true)
   Author toEntity(AuthorDto dto);
 
+
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "books" ,ignore = true)
+  void updateBookFromDto(AuthorDto dto, @MappingTarget Author entity);
 }
